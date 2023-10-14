adabf4366d77990ebe213a48d60dc54480390756
Prasanth K. Rajan <prasanth.k.rajan@gmail.com>
2023-10-14 18:01:46 +0800

    add search ability

--- app/controllers/api/v1/menus_controller.rb
+++ app/controllers/api/v1/menus_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::MenusController < ApplicationController
 	def index
-		@menus = Menu.all
+		@menus = if params[:q].present?
+			Menu.where("name ILIKE ?", "%#{params[:q]}%")
+		else 
+			Menu.all
+		end
 		render json: @menus
 	end
 end
--- spec/requests/get_menus_spec.rb
+++ spec/requests/get_menus_spec.rb
@@ -2,17 +2,68 @@ require 'rails_helper'
 
 RSpec.describe 'Menus', type: :request do
   describe 'GET /index' do
-    before do
-      FactoryBot.create_list(:menu, 10)
-      get '/api/v1/menus'
-    end
-    
-    it 'returns all menus' do
-      expect(json.size).to eq(10)
+    context 'and if data is present' do
+      before do
+        FactoryBot.create_list(:menu, 10)
+        get '/api/v1/menus'
+      end
+      
+      it 'returns all menus' do
+        expect(json.size).to eq(10)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(:success)
+      end
     end
 
-    it 'returns status code 200' do
-      expect(response).to have_http_status(:success)
+    context 'but if no data is present' do
+      before do
+        get '/api/v1/menus'
+      end
+      
+      it 'returns no menu' do
+        expect(json.size).to eq(0)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe 'GET /index with search params' do
+    before do
+      FactoryBot.create_list(:menu, 5, name: 'Classic Pizza')
+      FactoryBot.create_list(:menu, 5, name: 'Modern Pizza')
+    end
+
+    context 'and if data that matches the query present' do
+      before do
+        get "/api/v1/menus?q=classic"
+      end
+
+      it 'returns all menus with the name classic' do
+        expect(json.size).to eq(5)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'and if data does not match the query present' do
+      before do
+        get "/api/v1/menus?q=random"
+      end
+
+      it 'returns no menu' do
+        expect(json.size).to eq(0)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
